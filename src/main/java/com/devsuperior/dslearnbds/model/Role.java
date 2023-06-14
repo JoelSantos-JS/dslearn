06b297611a4e048493415a75941863a7aa26d186
Joel Santos <joeltere9@gmail.com>
2023-06-14 15:18:22 -0300

    create model role

---
+++ src/main/java/com/devsuperior/dslearnbds/model/Role.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dslearnbds.model;
+
+public class Role {
+
+    private long id;
+    private String authority;
+
+    public Role() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+}
