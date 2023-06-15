6cf058dc32f5ddd148a1157c567a99bf65505ae1
Joel Santos <joeltere9@gmail.com>
2023-06-15 13:14:55 -0300

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+
+INSERT INTO tb_course(name, img_Uri,img_Gray_Uri) VALUES ('Spring Boot', 'spring-boot.png','spring-boot-gray.png');
+INSERT INTO tb_course(name, img_Uri,img_Gray_Uri) VALUES ('Angular', 'angular.png','angular-gray.png');
